26d28070d027740fab40d2939c823d321435a4d4
yellowsub12 <fantasm1337@gmail.com>
2023-04-15 18:02:55 -0400

    added client upload feature

--- Client/client.py
+++ Client/client.py
@@ -7,29 +7,80 @@ PORT = 65432
 # set the path to the directory where the files will be saved
 file_dir = "Client"
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    print(f"Connected to server at {HOST}:{PORT}!")
+# create the client socket
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    while True:
-        # get the name of the file to be requested
-        file_name = input("Enter the name of the file to be requested: ")
+# connect to the server
+s.connect((HOST, PORT))
 
-        # send the file name to the server
+print(f"Connected to server at {HOST}:{PORT}!")
+
+while True:
+    # ask the user to input the action to be taken (download or upload)
+    action = input("Enter 'download' to download a file or 'upload' to upload a file: ")
+
+    # send the action to the server
+    s.sendall(action.encode("utf-8"))
+
+    if action.lower() == "download":
+        # ask the user to input the name of the file to be downloaded
+        file_name = input("Enter the name of the file to be downloaded: ")
+
+        # send the name of the file to the server
         s.sendall(file_name.encode("utf-8"))
 
-        # receive the size of the file from the server
-        file_size = int(s.recv(1024).decode("utf-8"))
+        # receive the size of the file to be received
+        file_size_bytes = s.recv(1024)
+        file_size = int.from_bytes(file_size_bytes, byteorder="big")
 
-        if file_size < 0:
-            print(f"ERROR: {s.recv(1024).decode('utf-8')}")
-            continue
+        # receive the contents of the file to be received
+        file_contents = bytearray()
+        bytes_received = 0
+        while bytes_received < file_size:
+            chunk = s.recv(min(4096, file_size - bytes_received))
+            file_contents.extend(chunk)
+            bytes_received += len(chunk)
 
-        # receive the file contents from the server
-        file_contents = s.recv(file_size)
-
-        # save the file contents to disk
-        file_path = os.path.join(file_dir, file_name)
+        # save the contents of the file to disk
+        file_path = file_dir + "/" + file_name
         with open(file_path, "wb") as f:
             f.write(file_contents)
-        print(f"File '{file_name}' ({file_size} bytes) has been saved to {file_path}!")
+
+        print(f"File '{file_name}' ({file_size} bytes) has been downloaded and saved in {file_dir}!")
+
+    elif action.lower() == "upload":
+        # ask the user to input the name of the file to be uploaded
+        file_name = input("Enter the name of the file to be uploaded: ")
+
+        # check if the file exists
+        file_path = file_dir + "/" + file_name
+        if not os.path.exists(file_path):
+            print(f"Error: file '{file_name}' does not exist in {file_dir}!")
+            continue
+
+        # send the name of the file to the server
+        s.sendall(file_name.encode("utf-8"))
+
+        # send the size of the file to be uploaded
+        file_size = os.path.getsize(file_path)
+        s.sendall(file_size.to_bytes(8, byteorder="big"))
+
+        # send the contents of the file to be uploaded
+        with open(file_path, "rb") as f:
+            while True:
+                chunk = f.read(4096)
+                if not chunk:
+                    break
+                s.sendall(chunk)
+
+        # receive a response from the server
+        response = s.recv(1024).decode("utf-8")
+        if response.lower() == "ok":
+            print(f"File '{file_name}' ({file_size} bytes) has been uploaded to the server!")
+        else:
+            print(f"Error: {response}")
+    else:
+        print(f"Invalid action '{action}'! Please enter 'download' or 'upload'.")
+        continue
+
+s.close()
--- Server/server.py
+++ Server/server.py
@@ -4,36 +4,71 @@ import os
 HOST = "127.0.0.1"
 PORT = 65432
 
-# set the path to the directory containing the files
+# set the path to the directory where the files will be saved
 file_dir = "Server"
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    print(f"Server is awaiting connection at {HOST}:{PORT}! Make sure to turn on the client connection!")
+# create the server socket
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# bind the socket to the host and port
+s.bind((HOST, PORT))
+
+# start listening for incoming connections
+s.listen()
+
+print(f"Server is awaiting connection at {HOST}:{PORT}! Make sure to turn on the client connection!")
+
+while True:
+    # accept a new connection
     conn, addr = s.accept()
     print(f"Server has connected! Connected to {addr}!")
+
     with conn:
-        print(f"Connected by {addr}")
         while True:
-            data = conn.recv(1024).decode("utf-8")
-            if not data:
-                break
+            # receive the action to be taken (download or upload)
+            action = conn.recv(1024).decode("utf-8")
 
-            # check if the requested file exists
-            file_path = os.path.join(file_dir, data)
-            if not os.path.exists(file_path):
-                conn.sendall("ERROR: File does not exist".encode("utf-8"))
+            if action.lower() == "download":
+                # receive the name of the file to be sent
+                file_name = conn.recv(1024).decode("utf-8")
+
+                # check if the file exists
+                file_path = os.path.join(file_dir, file_name)
+                if not os.path.exists(file_path):
+                    conn.sendall("-1".encode("utf-8"))
+                    conn.sendall(f"File '{file_name}' does not exist in {file_dir}!".encode("utf-8"))
+                    continue
+
+                # send the size of the file to be sent
+                file_size = os.path.getsize(file_path)
+                conn.sendall(str(file_size).encode("utf-8"))
+
+                # send the contents of the file to be sent
+                with open(file_path, "rb") as f:
+                    file_contents = f.read()
+                conn.sendall(file_contents)
+
+                print(f"File '{file_name}' ({file_size} bytes) has been sent to {addr}!")
+
+            elif action.lower() == "upload":
+                # receive the name of the file to be uploaded
+                file_name = conn.recv(1024).decode("utf-8")
+
+                # receive the size of the file to be uploaded
+                file_size = int(conn.recv(1024).decode("utf-8"))
+
+                # receive the contents of the file to be uploaded
+                file_contents = conn.recv(file_size)
+
+                # save the contents of the file to disk
+                file_path = os.path.join(file_dir, file_name)
+                with open(file_path, "wb") as f:
+                    f.write(file_contents)
+
+                print(f"File '{file_name}' ({file_size} bytes) has been uploaded by {addr}!")
+
+                # send a response to the client
+                conn.sendall("OK".encode("utf-8"))
+            else:
+                conn.sendall(f"Invalid action '{action}'! Please enter 'download' or 'upload'.".encode("utf-8"))
                 continue
-
-            # open the requested file and read its contents
-            with open(file_path, "rb") as f:
-                file_contents = f.read()
-
-            # send the file size to the client
-            file_size = len(file_contents)
-            conn.sendall(str(file_size).encode("utf-8"))
-
-            # send the file contents to the client
-            conn.sendall(file_contents)
-            print(f"File '{data}' ({file_size} bytes) has been sent to the client!")
