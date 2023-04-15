2460eb9ff815882ae91b0c507812e543c9f06662
yellowsub12 <fantasm1337@gmail.com>
2023-04-15 16:31:46 -0400

    added text file sharing ability between client and server

--- Client/client.py
+++ Client/client.py
@@ -1,13 +1,12 @@
-# app-client.py
 import socket
 import sys
 import json
-import uuid
-# Source for lots of code and comments here https://realpython.com/python-sockets/
+import os
 
 HOST = "127.0.0.1" # The server's hostname or IP address
 PORT = 65432 # The port used by the server
 
+CLIENT_DIR = "Client" # Directory where the file will be saved
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     addr = (HOST,PORT)
@@ -15,42 +14,24 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST,PORT))
     connection = True
     while connection:
-        RFW = input('Give me your name : ')
-        receivedRequest = json.dumps(RFW)
+        request = input("Enter the name of the file you want to request from the server (e.g. hello.txt): ")
+        s.sendall(json.dumps(request).encode("utf-8"))
 
-
-        s.sendall(receivedRequest.encode("utf-8"))
-        
-        print("\nRequest Sent!")
-        print(receivedRequest)
-        print("\nWaiting for Response ...\n")
-
-        # Receive Response from Server
-        # 1024 Represents Buffer Size in Bytes
         data = s.recv(1024)
-        
-        res = json.loads(data.decode('utf-8'))
 
-        # Print Response
-        print("Response Received!")
-        print(res)
+        if data.startswith(b"Error:"):
+            error = data.decode("utf-8").strip()
+            print(error)
+        else:
+            file_path = os.path.join(CLIENT_DIR, request)
+            with open(file_path, "wb") as f:
+                f.write(data)
+            print(f"{request} received and saved in {CLIENT_DIR}")
+
         user_input = input('Would you like to continue or end the program? Press 9 to exit! Else press 1 to continue : ')
         if user_input == '9':
             connection = False
         else:
             continue
 
-
-
-   
-
-
     print("\nClient Socket Closed!\n")
-    
-
-print(f"Client Received back {data!r}")
-
-#this creates a socket object
-#uses .connect() to connect to the server
-#uses .sendall() to send its messages
-#uses .recv() to read the server's reply and then prints it.
